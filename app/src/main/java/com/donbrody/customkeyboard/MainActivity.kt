b9a281cc0b9ca6b940bbd97117446b372e2852e1
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-11-26 12:53:42 -0800

    upgrade to AndroidX; add firebase dependencies

--- app/src/main/java/com/donbrody/customkeyboard/MainActivity.kt
+++ app/src/main/java/com/donbrody/customkeyboard/MainActivity.kt
@@ -2,7 +2,7 @@ package com.donbrody.customkeyboard
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import com.donbrody.customkeyboard.components.keyboard.CustomKeyboardView
